004baf4ba73de6beacb399f8decd53d2561b17f7
Dmitry Fedyuk <dfediuk@gmail.com>
2019-01-31 17:08:26 +0800

    no message

--- app/code/local/Ecpay/Ecpaypayment/Block/Form/Ecpaypayment.php
+++ app/code/local/Ecpay/Ecpaypayment/Block/Form/Ecpaypayment.php
@@ -5,6 +5,8 @@ class Ecpay_Ecpaypayment_Block_Form_Ecpaypayment extends Mage_Payment_Block_Form
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('ecpaypayment/form/payment.phtml');
+        $this->setTemplate('ecpaypayment/form/payment.phtml')
+		->setMethodTitle($this->__('Visa/Mastercard by ECPay'))
+	;
     }
 }
